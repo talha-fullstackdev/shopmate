948b50fbf57f22ce97841b139fe4c7a8899b9573
Talha-TN <nawazt964@gmail.com>
2025-01-20 17:05:16 -0800

    removes unnecessary comented code

--- src/components/CartAnimate.js
+++ src/components/CartAnimate.js
@@ -7,12 +7,14 @@ export const CartAnimate = () => {
   const dispatch = useDispatch();
   const products = useSelector((state) => state.CartSlice.cartList);
   const total = useSelector((state) =>
-    state.CartSlice.cartList.reduce((total, product) => total + product.price, 0)
+    state.CartSlice.cartList.reduce(
+      (total, product) => total + product.price,
+      0
+    )
   );
   const [isVisible, setIsVisible] = useState(false);
   useEffect(() => {
     if (products.length > 0) {
-    
       const timer = setTimeout(() => {
         setIsVisible(false);
       }, 3000);
@@ -24,24 +26,24 @@ export const CartAnimate = () => {
     }
   }, [products]);
   return (
-    <div className={`cart-animate-wrapper ${isVisible && "show" }`}>
+    <div className={`cart-animate-wrapper ${isVisible && "show"}`}>
       <section className="cart">
         <div className="cart-items">
           <h1>
             Cart Items: {products.length} / ${total.toFixed(2)}
           </h1>
           {products.map((product) => (
-    
             <div className="cart-item" key={product.id}>
               <img src={product.image} alt={product.name} />
               <div className="cart-item-details">
-                <p className="product-name" >{product.name}</p>
-            
-                  <p className="cart-item-color">Color: {product.selectedColor}</p>
-            
-           
-                  <p className="cart-item-color">Type: {product.selectedType}</p>
-            
+                <p className="product-name">{product.name}</p>
+
+                <p className="cart-item-color">
+                  Color: {product.selectedColor}
+                </p>
+
+                <p className="cart-item-color">Type: {product.selectedType}</p>
+
                 <button
                   onClick={() => dispatch(remove(product))}
                   className="delete-btn"
--- src/index.js
+++ src/index.js
@@ -7,7 +7,6 @@ import { ToastContainer } from "react-toastify";
 import "react-toastify/dist/ReactToastify.css";
 import App from "./App";
 import "./index.css";
-
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <React.StrictMode>
