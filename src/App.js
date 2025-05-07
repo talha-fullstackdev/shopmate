88de94a0e68cb7aea48b083929a6b4b694de14a7
talha-fullstackdev <talhafullstack786@gmail.com>
2025-05-07 14:07:43 +0500

    added app.js file

--- src/App.js
+++ src/App.js
@@ -1,14 +1,13 @@
 import { AllRoutes } from "./routes/AllRoutes";
 import { Header } from "./components";
 import { CartAnimate } from "./components/CartAnimate";
-import './App.css';
+import "./App.css";
 function App() {
   return (
     <div className="App">
       <Header />
-      <CartAnimate/>
+      <CartAnimate />
       <AllRoutes />
-
     </div>
   );
 }
